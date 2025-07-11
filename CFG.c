87979c2004ca9da37f26a73248eeb50b753fdd3d
Fazal Sayed <syedfazal2956@gmail.com>
2025-07-11 09:54:31 +0530

    CFG.c

--- CFG.c
+++ CFG.c
@@ -24,3 +24,25 @@ int main() {
     }
     return 0;
 }
+
+Output:
+How many states in the NFA with e-moves: 3
+How many symbols in the input alphabet including e: 3
+Enter the symbols without space. Give 'e' first: e01
+How many transitions from state 0 for the input e: 1
+Enter state 1 for transition 1 from state 0 on input e: 1
+How many transitions from state 0 for the input 0: 0
+How many transitions from state 0 for the input 1: 1
+Enter state 1 for transition 1 from state 0 on input 1: 1
+How many transitions from state 1 for the input e: 1
+Enter state 1 for transition 1 from state 1 on input e: 2
+How many transitions from state 1 for the input 0: 2
+Enter state 1 for transition 1 from state 1 on input 0: 0
+Enter state 2 for transition 2 from state 1 on input 0: 1
+How many transitions from state 1 for the input 1: 0
+How many transitions from state 2 for the input e: 0
+How many transitions from state 2 for the input 0: 0
+How many transitions from state 2 for the input 1: 0
+e-closure(0) = {0, 1, 2}
+e-closure(1) = {1, 2}
+e-closure(2) = {2}
